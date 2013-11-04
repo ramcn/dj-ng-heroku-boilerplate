104955dad24ba2a9f991967af4be91fb5f82ff0b
Tim Jagodzinski <tim.jagodzinski@gmail.com>
2013-11-04 13:09:32 +0100

    Add home view serving the SPA.

--- backend/views.py
+++ backend/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def home(request, template_name="index.html"):
+    """
+    The index view which serves the Angular.js SPA. The path to the correct
+    index.html is configured in /config/settings.py
+    """
+    return render_to_response(template_name,
+                              context_instance=RequestContext(request))
