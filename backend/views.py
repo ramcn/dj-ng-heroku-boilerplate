121c29aef3938f3da123452d8cd1063f0734b3d7
Tim Jagodzinski <tim.jagodzinski@gmail.com>
2013-11-05 16:46:05 +0100

    Format to PEP8.

--- backend/views.py
+++ backend/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+
 def home(request, template_name="index.html"):
     """
     The index view which serves the Angular.js SPA. The path to the correct
-    index.html is configured in /config/settings.py
+    index.html is configured in /config/settings.py with the SPA_INDEX
+    variable.
     """
     return render_to_response(template_name,
                               context_instance=RequestContext(request))
