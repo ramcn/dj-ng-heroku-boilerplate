de3dffc6ebf123459ce3d532f6a29088e2c64826
Tim Jagodzinski <tim.jagodzinski@gmail.com>
2013-11-18 10:58:15 +0100

    Minor tweaks.

--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-
 def home(request, template_name="index.html"):
     """
     The index view which serves the Angular.js SPA. The path to the correct
